d3762ef4c5c6be99f7960f41381b0500b3200f4b
cooklee <sbbogus@gmail.com>
2024-02-05 14:07:43 +0100

    initial commit

---
+++ BookShelfONLS17/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for BookShelfONLS17 project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path
+from bookshelf import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('szablon/', views.index_z_szablonem, name='index_z_szablonem'),
+    path('losuj/', views.losuj, name='losuj'),
+    path('losuj_liczby/<int:ilosc>/', views.losuj_6, name='losuj_6'),
+    path('nazwisko/<int:index>/', views.get_name, name='nazwiska'),
+    path('add_nazwisko/<str:name>/', views.add_name_by_url, name='add_name_by_url')
+]
---
+++ bookshelf/views.py
@@ -0,0 +1,58 @@
+from random import randint
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+nazwiska = [
+    'Bogusłąwski',
+    'Gąsek',
+    'Kowalik',
+    'Gaw2roniak',
+    'grabiszyn',
+    'tomczak'
+]
+def index(request):
+    return HttpResponse("Hello World")
+
+
+def index_z_szablonem(request):
+    return render(request, 'index.html')
+
+
+def losuj(request):
+    a = randint(1, 100)
+    return render(request, 'losuj.html', {'liczba':a})
+
+def losuj_6(request, ilosc):
+    lst = []
+    for _ in range(ilosc):
+        lst.append(randint(1,100))
+    return render(request, 'losuj.html', {'liczba':lst})
+
+def losuj_7(request):
+    return render(request, 'jakis_szablon.html')
+
+
+def get_name(request, index):
+    if index < len(nazwiska):
+        nazwisko = f"wybrano {nazwiska[index]}"
+    else:
+        nazwisko = "index jest za duży"
+    return render(request, 'list.html', {'nazwisko':nazwisko})
+
+
+def add_name_by_url(request, name):
+    """
+    mozna w ten sposób ale nie radze :D
+
+    """
+    nazwiska.append(name)
+    return render(request, 'success.html')
+
+
+def add_name(request):
+    if request.method == "POST":
+        last_name = request.POST.get('last_name')
+        nazwiska.append(last_name)
+    return render(request, 'add_name.html')
